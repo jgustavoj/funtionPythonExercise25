779ab4923863cd4242ba223fc6c7ce773ef7bd99
jgustavoj <gjime011@fiu.edu>
2020-10-20 01:15:43 +0000

    6

--- exercises/06-lambda-functions/app.py
+++ exercises/06-lambda-functions/app.py
@@ -1,6 +1,5 @@
 # your function here
 
-is_odd = lambda num: num % 2 == 0
-
-result = is_odd(2)
-print(result)
+is_odd = lambda num: num % 2 != 0
+#code test
+print(is_odd(3))
--- exercises/07-lambda-function-two/app.py
+++ exercises/07-lambda-function-two/app.py
@@ -1,5 +1,4 @@
 
 
-
 # From this line above, plese do not change code below
-print(rapid("bob")) #should print bo
+print(rapid("bob"))  # should print bo
