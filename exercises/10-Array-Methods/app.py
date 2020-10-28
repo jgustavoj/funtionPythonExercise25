885b91a32a740a126210d59c6683f5fe8f1754c2
jgustavoj <gjime011@fiu.edu>
2020-10-28 15:37:59 +0000

    10

--- exercises/10-Array-Methods/app.py
+++ exercises/10-Array-Methods/app.py
@@ -1,6 +1,7 @@
 names = ['John', 'Kenny', 'Tom', 'Bob', 'Dilan']
 ## CREATE YOUR FUNCTION HERE
-def sort_names(alpha):
-    return alpha.sort() 
+def sort_names(name):
+    name.sort()
+    return name 
 
 print(sort_names(names))
